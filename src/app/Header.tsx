4d427d8ca42797967ea2534116510e85f5841e3b
JuanPrato <pratojuanmanuel2@gmail.com>
2023-06-19 20:20:32 -0300

    check glass on header

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -2,6 +2,41 @@
 
 import Link from "next/link";
 import { useSession } from "@/hook/useSession";
+import { twMerge } from "tailwind-merge";
+
+enum BG_PERCETAGES {
+  DEAD = 10,
+  DANGER = 20,
+  WARNING = 60,
+  GOOD = 100
+}
+
+const BGS = {
+  [BG_PERCETAGES.GOOD]: "good",
+  [BG_PERCETAGES.WARNING]: "warning",
+  [BG_PERCETAGES.DANGER]: "danger",
+  [BG_PERCETAGES.DEAD]: "dead"
+}
+
+function getBudgetClasses(percentage: number) {
+
+  let bg: string;
+  const aux = Math.ceil(percentage / 10) * 10;
+  const showP: number = aux < 0 ? 100 : aux;
+
+  if (percentage <= BG_PERCETAGES.DEAD) {
+    bg = BGS[BG_PERCETAGES.DEAD];
+  } else if (percentage <= BG_PERCETAGES.DANGER) {
+    bg = BGS[BG_PERCETAGES.DANGER];
+  } else if (percentage <= BG_PERCETAGES.WARNING) {
+    bg = BGS[BG_PERCETAGES.WARNING];
+  } else {
+    bg = BGS[BG_PERCETAGES.GOOD];
+  }
+
+  return `before:bottom-[${showP}%] after:bottom-[${showP}%] bg-${bg}`
+
+}
 
 export default function Header() {
 
@@ -23,8 +58,8 @@ export default function Header() {
       {
         !!profile?.linkProfile && (
           <div className="flex items-center justify-center gap-5 py-2">
-            <h2 className="text-2xl font-semibold text-white">{profile?.linkProfile?.username}</h2>
-            <div className="bg-warning before:bg-secondary after:bg-secondary before:bg-opacity-60 ring ring-borderGlass water h-10 w-10 rounded-full after:bottom-[50%] before:bottom-[50%]" />
+            <h2 className="text-2xl font-semibold text-white">{profile?.linkProfile.username}</h2>
+            <div className={twMerge("bg-warning before:bg-secondary after:bg-secondary before:bg-opacity-60 ring ring-borderGlass water h-10 w-10 rounded-full", getBudgetClasses(Math.ceil((profile.linkProfile.current / profile.linkProfile.total) * 100)))} />
           </div>
         )
       }
