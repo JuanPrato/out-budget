ff0894694e6bf1c768bbd995181516f9d7c55b17
JuanPrato <pratojuanmanuel2@gmail.com>
2023-06-19 20:20:51 -0300

    change checkbox button styles

--- src/app/Inputs.tsx
+++ src/app/Inputs.tsx
@@ -9,7 +9,7 @@ export function IncomeButton() {
   const [value, setValue] = useState(false);
 
   return (
-    <button className={twMerge("bg-danger p-5 rounded-xl text-white flex gap-3 justify-center transition-colors font-bold col-span-3", value && "bg-good text-black")} onClick={() => setValue(v => !v)} type="button">INGRESO <input checked={value} type="checkbox" name="income" id="" className="w-[20px] h-[20px]" /></button>
+    <button className={twMerge("bg-danger p-5 rounded-xl text-white flex gap-3 justify-center transition-colors font-bold col-span-3", value && "bg-good text-black")} onClick={() => setValue(v => !v)} type="button">{value ? "INGRESO" : "EGRESO"}<input checked={value} type="hidden" name="income" id="" className="w-[20px] h-[20px]" /></button>
   );
 }
 
@@ -18,7 +18,7 @@ export function IncomeInput() {
   return (
     <>
       <span className="col-span-1 text-3xl grid place-items-center font-bold">$</span>
-      <input type="text" placeholder="2500" name="quantity" className="col-span-5 p-3 text-xl rounded-lg font-semibold block text-black" />
+      <input type="text" placeholder="2500" name="quantity" className="col-span-5 p-3 text-xl rounded-lg font-semibold block text-black" autoComplete="off" />
     </>
   );
 
