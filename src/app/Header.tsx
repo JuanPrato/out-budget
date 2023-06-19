8872030af56ac2bb5561814284110eb72efe2b01
JuanPrato <pratojuanmanuel2@gmail.com>
2023-06-19 20:02:59 -0300

    check for linked profile

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -20,10 +20,14 @@ export default function Header() {
           className="bg-primary p-5 rounded-xl text-bold text-white"
         >SALIR</button>
       </div>
-      <div className="flex items-center justify-center gap-5 py-2">
-        <h2 className="text-2xl font-semibold text-white">{profile?.linkProfile?.username}</h2>
-        <div className="bg-warning before:bg-secondary after:bg-secondary before:bg-opacity-60 ring ring-borderGlass water h-10 w-10 rounded-full after:bottom-[50%] before:bottom-[50%]" />
-      </div>
+      {
+        !!profile?.linkProfile && (
+          <div className="flex items-center justify-center gap-5 py-2">
+            <h2 className="text-2xl font-semibold text-white">{profile?.linkProfile?.username}</h2>
+            <div className="bg-warning before:bg-secondary after:bg-secondary before:bg-opacity-60 ring ring-borderGlass water h-10 w-10 rounded-full after:bottom-[50%] before:bottom-[50%]" />
+          </div>
+        )
+      }
     </header>
   )
 }
