01345545e4ea872e6491c204ee1ebc488434db5a
JuanPrato <pratojuanmanuel2@gmail.com>
2023-06-20 11:23:41 -0300

    better type income button

--- src/app/Inputs.tsx
+++ src/app/Inputs.tsx
@@ -9,17 +9,17 @@ export function IncomeButton() {
   const [value, setValue] = useState(false);
 
   return (
-    <button className={twMerge("bg-danger p-5 rounded-xl text-white flex gap-3 justify-center transition-colors font-bold col-span-3", value && "bg-good text-black")} onClick={() => setValue(v => !v)} type="button">{value ? "INGRESO" : "EGRESO"}<input checked={value} type="hidden" name="income" id="" className="w-[20px] h-[20px]" /></button>
+    <button className={twMerge("bg-danger p-5 rounded-xl text-white flex gap-3 justify-center transition-colors font-bold col-span-3", value && "bg-good text-black")} onClick={() => setValue(v => !v)} type="button">{value ? "INGRESO" : "EGRESO"}<input checked={value} type="hidden" name="income" readOnly className="w-[20px] h-[20px]" /></button>
   );
 }
 
 export function IncomeInput() {
 
   return (
-    <>
-      <span className="col-span-1 text-3xl grid place-items-center font-bold">$</span>
-      <input type="text" placeholder="2500" name="quantity" className="col-span-5 p-3 text-xl rounded-lg font-semibold block text-black" autoComplete="off" />
-    </>
+    <div className="flex w-full col-span-6 bg-white rounded-lg">
+      <span className="min-w-[50px] text-2xl grid place-items-center font-bold">$</span>
+      <input type="text" placeholder="2500" name="quantity" className="p-3 text-2xl rounded-lg font-semibold block text-black w-full focus:outline-none" autoComplete="off" />
+    </div>
   );
 
 }
