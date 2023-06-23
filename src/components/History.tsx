ddc7b57654d8d644f42dd7ffd269d5e24d947bc6
JuanPrato <pratojuanmanuel2@gmail.com>
2023-06-23 13:43:10 -0300

    hidden history title on close

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -21,7 +21,7 @@ export default function History({ getHistory, show, onClose }: { getHistory: () 
 
   return (
     <div className={twMerge("bg-transparent absolute bottom-0 h-screen w-full z-30 transition-[max-width] duration-300 flex overflow-hidden max-w-[0px]", show && "max-w-full")}>
-      <div className="w-[80%] sm:w-[70%] md:w-[50%] max-w-[400px] h-full bg-primary flex flex-col py-4 shadow-2xl">
+      <div className="w-[80%] sm:w-[70%] md:w-[50%] max-w-[400px] h-full bg-primary flex flex-col py-4 shadow-2xl overflow-hidden">
         <h2 className="text-3xl my-2 text-white shadow-lg pb-4 pl-2">HISTORIAL</h2>
         <ul className="text-2xl w-full font-semibold h-full overflow-y-auto overflow-x-hidden
         ">
